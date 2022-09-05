fc15606458de65e9498b66df17984ab614221b1f
Humble.X <Humble.Xiang@gmail.com>
2022-09-05 11:14:27 +0800

    build: project initial

---
+++ pages/api/hello.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const hello = (_: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ text: 'Hello' });
+};
+
+export default hello;
